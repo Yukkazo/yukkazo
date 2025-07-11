3a15eb82ca35be15c4c0c349897eeb34cf1d8b81
Onel <113157468+onelmachiz@users.noreply.github.com>
2025-07-11 14:28:47 -0400

    Add files via upload

---
+++ account_followup_ykz/__manifest__.py
@@ -0,0 +1,14 @@
+##############################################################################
+{
+    'name': 'Payment Follow-up Management YKZ',
+    'author': 'Onel',
+    'license': 'OEEL-1',
+    'category': 'Accounting/Accounting',
+    'data': [],
+    'depends': [
+        'account', 'account_followup',
+    ],
+    'installable': True,
+    'test': [],
+    'version': "1.2",
+}
